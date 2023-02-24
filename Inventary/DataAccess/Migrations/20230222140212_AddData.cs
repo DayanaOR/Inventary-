3f6dedaa75db62304c5cad9e6c4789176a1a57c9
Dayana Olarte <kellyolarter23@gmail.com>
2023-02-24 10:34:46 -0500

    Arreglar carpeta

---
+++ Inventary/DataAccess/Migrations/20230222140212_AddData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { "ASH", "Aseo hogar" },
+                    { "BLZ", "Belleza" },
+                    { "HGR", "Hogar" },
+                    { "PER", "Perfumeria" },
+                    { "SLD", "Salud" },
+                    { "VDJ", "Video Juegos" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Warehouse",
+                columns: new[] { "WarehouseId", "WarehouseAddress", "WarehouseName" },
+                values: new object[,]
+                {
+                    { "9046152e-1be7-480f-9673-dc8a1fefe048", "Kra. 14-78", "Bodega Occidental" },
+                    { "f5477acb-62f5-4ad5-aefc-0f204c9618f7", "AV.112 #67-90", "Bodega Oriental" },
+                    { "fd9facf2-ce1a-42be-99a3-d538acfc9ba9", "Calle 26-98", "Bodega Central" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "ASH");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "BLZ");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "HGR");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "PER");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "SLD");
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "CategoryId",
+                keyValue: "VDJ");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouse",
+                keyColumn: "WarehouseId",
+                keyValue: "9046152e-1be7-480f-9673-dc8a1fefe048");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouse",
+                keyColumn: "WarehouseId",
+                keyValue: "f5477acb-62f5-4ad5-aefc-0f204c9618f7");
+
+            migrationBuilder.DeleteData(
+                table: "Warehouse",
+                keyColumn: "WarehouseId",
+                keyValue: "fd9facf2-ce1a-42be-99a3-d538acfc9ba9");
+        }
+    }
+}
